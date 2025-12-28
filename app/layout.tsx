9d340349d2514d8f2785e7d93c523df055e25469
Klyde Apostol <apostolklyde04@gmail.com>
2025-12-28 15:18:16 +0800

    added domain ownership

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+// DELETE THIS LINE: import "./googleefe58c37a587b98b.html";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "BSCS Calendar",
   description: "BSCS Batch 2025 Exclusive Calendar Automation",
+  verification: {
+    google: "googleefe58c37a587b98b.html",
+  },
 };
 
 export default function RootLayout({
